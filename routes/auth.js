cc66088c3bcb3a633b7873850688dc876afc54d4
toptalentdev <toptal715@gmail.com>
2022-10-23 00:25:08 -0400

    add call back url for twitter signin

--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/google', authController.glogin);
 router.post('/facebook', authController.flogin);
 router.post('/twitter', authController.getTwitterLoginUrl);
+router.get('/twitter/callback', authController.tlogin);
 
 module.exports = router;
